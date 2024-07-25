05f33dc3437ab3b56f8b782248bfe4479dc44dc0
Maksym Kovalenko <maxeladze@gmail.com>
2024-07-25 10:19:07 +0200

    Add database seeder with products & systems

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Product;
+use App\Models\System;
 
 class DatabaseSeeder extends Seeder
 {
@@ -19,5 +21,41 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        // Create Products
+        $solarPanel = Product::create([
+            'name' => 'Solar Panel',
+            'stock' => 1000,
+            'initial_stock' => 1000,
+            'low_stock_email_sent' => false,
+        ]);
+
+        $inverter = Product::create([
+            'name' => 'Inverter',
+            'stock' => 100,
+            'initial_stock' => 100,
+            'low_stock_email_sent' => false,
+        ]);
+
+        $optimizer = Product::create([
+            'name' => 'Optimizer',
+            'stock' => 500,
+            'initial_stock' => 500,
+            'low_stock_email_sent' => false,
+        ]);
+
+        // Create First System
+        $basicSystem = System::create(['name' => 'Basic System']);
+        // Attach products to the first system
+        $basicSystem->products()->attach($solarPanel->id, ['quantity' => 12]);
+        $basicSystem->products()->attach($inverter->id, ['quantity' => 1]);
+        $basicSystem->products()->attach($optimizer->id, ['quantity' => 12]);
+
+        // Create Second System
+        $advancedSystem = System::create(['name' => 'Advanced System']);
+        // Attach products to the second system
+        $advancedSystem->products()->attach($solarPanel->id, ['quantity' => 24]);
+        $advancedSystem->products()->attach($inverter->id, ['quantity' => 2]);
+        $advancedSystem->products()->attach($optimizer->id, ['quantity' => 24]);
     }
 }
